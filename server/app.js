7a16ae0f3339d100d3983fd0080df739083fdce3
Isaac Halvorson <hello@hisaac.net>
2016-11-23 16:31:48 -0600

    build basic app routing

---
+++ server/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+//----------------------------------------------------------------------------//
+
+
+
+// static files
+app.use(express.static('./server/public'));
+
+app.listen(3000);
