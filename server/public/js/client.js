f49d5fe1e8b51256583dbf5ad91bb14032f784e6
Isaac Halvorson <hello@hisaac.net>
2016-11-26 13:44:27 -0600

    move form into table template file

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -13,8 +13,21 @@ app.controller('TableController', ['$http', function ($http) {
   var self = this;
   $http.get('/db')
     .then(function (res) {
+      self.totalMonthlyExpenditures = 0;
       self.data = res.data;
+
+      //loops through data and adds all salaries to 'totalMonthlyExpenditures'
+      for (var i = 0; i < res.data.length; i++) {
+        self.totalMonthlyExpenditures += Number(res.data[i].salary.replace(/[^0-9\.]+/g,""));
+      }
+
+      //calculates average by dividing by total salary items
+      self.totalMonthlyExpenditures /= i;
     });
+
+  var submitEmployee = function(){
+    
+  };
 }]);
 
 // app.controller('FormController', ['$http', function ($http) {
